f36363610566a1c9298c8263162ece69f14837e4
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-07-20 10:49:35 -0400

    initial commit

---
+++ apps/time-tracking-dashboard/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'time-tracking-dashboard',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/time-tracking-dashboard',
+};
