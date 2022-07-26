e24a71ce16df624322e2ac27572620a19e257ffa
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-07-26 17:09:23 -0400

    time-tracking-dashboard: add breakpoints
    time-tracking-dashboard: update breakpoints

---
+++ apps/time-tracking-dashboard/src/styles/breakpoints.ts
@@ -0,0 +1,9 @@
+const size = {
+  tabletAndUp: 620,
+  tabletLargerAndUp: 900,
+  desktopAndUp: 1170,
+};
+
+export const tabletAndUp = `@media (min-width: ${size.tabletAndUp}px)`;
+export const tabletLargerAndUp = `@media (min-width: ${size.tabletLargerAndUp}px)`;
+export const desktopAndUp = `@media (min-width: ${size.desktopAndUp}px)`;
