87b84625f96f3aba16ecae04aef3bd916a836d4e
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-07-29 15:58:10 -0400

    root: add frontend-mentor apps folder

--- apps/frontend-mentor/time-tracking-dashboard/jest.config.ts
+++ apps/frontend-mentor/time-tracking-dashboard/jest.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'time-tracking-dashboard',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/time-tracking-dashboard',
+  coverageDirectory:
+    '../../../coverage/apps/frontend-mentor/time-tracking-dashboard',
 };
