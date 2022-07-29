24646744235eb4859727bebc43483ffb2de7c446
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2022-07-29 11:03:57 -0400

    time-tracking-dashboard: update main page

--- apps/time-tracking-dashboard/src/app/app.tsx
+++ apps/time-tracking-dashboard/src/app/app.tsx
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+import UserActivityDashboard from '@components/UserActivityDashboard';
+import { tabletAndUp } from '@styles/breakpoints';
 
 export function App() {
   return (
-    <StyledApp>
-      <h1>Time Tracking Dashboard</h1>
-    </StyledApp>
+    <Page>
+      <UserActivityDashboard />
+    </Page>
   );
 }
 
 export default App;
+
+const Page = styled.div`
+  padding: 24px;
+  height: 100%;
+
+  ${tabletAndUp} {
+    padding: 30px;
+    display: grid;
+    place-items: center;
+  }
+`;
